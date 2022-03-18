255357f51d9b312f6d282285c6d47945ba6645d7
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-18 14:50:03 +0900

    [FIX] Contracts

--- src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
+++ src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
@@ -1,6 +1,6 @@
 package kr.hs.dgsw.gg.api.objects
 
 object Contracts {
-    const val BASE_URL = "https://kr.api.riotgames.com/lol/"
+    const val BASE_URL = "https://kr.api.riotgames.com/"
     const val API_KEY = ""
 }
