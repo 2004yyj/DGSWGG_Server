9c2babbc659f3425f29453e4c60379d94187a155
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-21 20:41:38 +0900

    [FIX] Modify MatchUserRepository Import

--- src/main/kotlin/kr/hs/dgsw/gg/repository/MatchUserRepository.kt
+++ src/main/kotlin/kr/hs/dgsw/gg/repository/MatchUserRepository.kt
@@ -1,10 +1,9 @@
 package kr.hs.dgsw.gg.repository
 
 import kr.hs.dgsw.gg.data.vo.MatchUserVO
-import kr.hs.dgsw.gg.data.vo.MatchVO
 import org.springframework.data.jpa.repository.JpaRepository
-import java.awt.print.Pageable
+import org.springframework.data.domain.Pageable
 
 interface MatchUserRepository: JpaRepository<MatchUserVO, String> {
-    fun getAllBySummonerId(summonerId: String, pageable: Pageable): List<MatchUserVO>
+    fun findAllBySummonerId(summonerId: String, pageable: Pageable): List<MatchUserVO>
 }
