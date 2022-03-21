af0a45c760a760fe444643c79d747675c620a04c
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-21 11:51:06 +0900

    [FIX] Modify Expired Api Key

--- src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
+++ src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
@@ -3,5 +3,5 @@ package kr.hs.dgsw.gg.api.objects
 object Contracts {
     const val KR_BASE_URL = "https://kr.api.riotgames.com/"
     const val ASIA_BASE_URL = "https://asia.api.riotgames.com/"
-    const val API_KEY = "RGAPI-fab137e4-1635-4173-ab7a-0ce0dd80f198"
+    const val API_KEY = "RGAPI-2623eeb4-d7ea-46da-9a89-68cf1973eb01"
 }
