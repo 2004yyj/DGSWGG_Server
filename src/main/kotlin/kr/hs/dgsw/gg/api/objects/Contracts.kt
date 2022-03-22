9ec333af2ef29003d1fd3fce6fe47d31be3bdd31
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-22 16:02:36 +0900

    [ADD] Asset 레트로핏 추가

--- src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
+++ src/main/kotlin/kr/hs/dgsw/gg/api/objects/Contracts.kt
@@ -4,4 +4,6 @@ object Contracts {
     const val KR_BASE_URL = "https://kr.api.riotgames.com/"
     const val ASIA_BASE_URL = "https://asia.api.riotgames.com/"
     const val API_KEY = "RGAPI-fa9eb4d9-a6b5-4898-806d-5dd4719bb2b0"
+    private const val DataDragon_VERSION = "12.5.1"
+    const val ASSETS_BASE_URL ="https://ddragon.leagueoflegends.com/cdn/${DataDragon_VERSION}/"
 }
--- src/main/kotlin/kr/hs/dgsw/gg/api/objects/RetrofitObject.kt
+++ src/main/kotlin/kr/hs/dgsw/gg/api/objects/RetrofitObject.kt
@@ -4,6 +4,7 @@ import kr.hs.dgsw.gg.api.AsiaRiotApi
 import kr.hs.dgsw.gg.api.KrRiotApi
 import kr.hs.dgsw.gg.api.apikey.ApiKeyInterceptor
 import kr.hs.dgsw.gg.api.objects.Contracts.ASIA_BASE_URL
+import kr.hs.dgsw.gg.api.objects.Contracts.ASSETS_BASE_URL
 import kr.hs.dgsw.gg.api.objects.Contracts.KR_BASE_URL
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -22,6 +23,12 @@ object RetrofitObject {
 
     val asiaRetrofit = Retrofit.Builder()
         .baseUrl(ASIA_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    val assetsRetrofit = Retrofit.Builder()
+        .baseUrl(ASSETS_BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .client(okHttpClient)
         .build()
